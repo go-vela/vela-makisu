7e610e046c2d272b62238dfce9f02f04ed3eb856
Jordan Brockopp <jordan.brockopp@target.com>
2021-03-01 12:52:54 -0600

    enhance: make version output consistent (#26)

--- cmd/vela-makisu/main.go
+++ cmd/vela-makisu/main.go
@@ -7,7 +7,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 	"time"
 
@@ -20,6 +19,19 @@ import (
 )
 
 func main() {
+	// capture application version information
+	v := version.New()
+
+	// serialize the version information as pretty JSON
+	bytes, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	// output the version information to stdout
+	fmt.Fprintf(os.Stdout, "%s\n", string(bytes))
+
+	// create new CLI application
 	app := cli.NewApp()
 
 	// Plugin Information
@@ -39,7 +51,7 @@ func main() {
 
 	app.Action = run
 	app.Compiled = time.Now()
-	app.Version = version.New().Semantic()
+	app.Version = v.Semantic()
 
 	// Plugin Flags
 
@@ -62,23 +74,14 @@ func main() {
 	// add global flags
 	app.Flags = append(app.Flags, globalFlags...)
 
-	err := app.Run(os.Args)
+	err = app.Run(os.Args)
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
 
 // run executes the plugin based off the configuration provided.
 func run(c *cli.Context) error {
-	// capture the version information as pretty JSON
-	v, err := json.MarshalIndent(version.New(), "", "  ")
-	if err != nil {
-		return err
-	}
-
-	// output the version information to stdout
-	fmt.Fprintf(os.Stdout, "%s\n", string(v))
-
 	// set the log level for the plugin
 	switch c.String("log.level") {
 	case "t", "trace", "Trace", "TRACE":
@@ -139,7 +142,7 @@ func run(c *cli.Context) error {
 	}
 
 	// validate the plugin
-	err = p.Validate()
+	err := p.Validate()
 	if err != nil {
 		return err
 	}
